e44986024b8249407700a6a02e4e3422105b3fb4
Jorge Andres Moreno Jaimes <jamper91a@gmail.com>
2020-03-20 18:47:09 +1300

    Implementing models for payments

---
+++ api/models/Offer.js
@@ -0,0 +1,49 @@
+/**
+ * Offer.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    amount: {
+      type: 'number',
+      required: true
+    },
+    bonus: {
+      type: 'number',
+      defaultsTo: 0
+    },
+    active: {
+      type: 'number',
+      required: true
+    },
+    options: {
+      type: 'json',
+      required: false
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    domain: {
+      model: 'domain'
+    },
+    gateway: {
+      model :'gateway'
+    }
+  },
+
+};
+
